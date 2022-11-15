cc397333ac07699685171c269e2d0d001846de51
Damian Thomas <dimitrisonic@gmail.com>
2022-11-15 14:39:08 -0500

    core functionality implemented - tests partially implemented

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,52 @@
-use clap::{Parser, CommandFactory, error::ErrorKind};
-use std::{fs, path::Path, process::{Command}};
-
-/// Copies and mass-renames files using the wildcard character (*)
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// The files to be copied and renamed.
-    #[clap(value_parser)]
-    files: Vec<String>,
-    /// The destination path for the renamed files
-    #[clap(last = true, required = true)]
-    destination: String
-}
-
-
-fn process_args(args: Args)  {
-    // get the list of paths
-    // use last path as destination
-	let mut output_files: Vec<String>;
-	if !args.destination.contains('*') {
-		Args::command().error(
-			ErrorKind::InvalidValue, 
-			format!(
-				"cpar only works on paths with wildcards. wildcard was not found in destination path {}", 
-				args.destination
-			)
-		).exit();
-	}
-
-	for filename in &args.files {
-		// assert!(filename.contains('*'));
-		println!("{}", filename.contains('*'));
-		if !filename.contains('*') {
-			Args::command().error(
-				ErrorKind::InvalidValue, 
-				format!(
-					"cpar only works on paths with wildcards. wildcard was not found in input path {}", 
-					filename
-				)
-			).exit();
-		}
-
-		let path = Path::new(&filename);
-		
-		if path.is_dir() {
-			Args::command().error(ErrorKind::InvalidValue, "cpar does not accept directories.").exit();
-		}	
-	}
-
-	println!("{:?}", args.files)
-    
-    // cpar src/ContactForm* src/MembershipForm* ->
-    // ContactForm.tsx -> MembershipForm.tsx
-    // ContactForm.scss -> MembershipForm.scss
-	// or
-	// cpar src/*.ts -> src/*.js
-	// script.ts -> script.js
-    // etc.
-}
+mod cli;
+use clap::Parser;
+use cli::{process_args, Args};
 
 fn main() {
     let args = Args::parse();
-	process_args(args)
-    // println!("Hello {:?}! I'm sending you to {:?}", args.files, args.destination)
+    process_args(args)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{process::{Command, Output}, io::Error, result::Result, fs};
+    use crate::cli::Args;
+
+	// runs cpar as a bash command to directly debug stderr and stdout
+    fn run_cpar(args: Args) -> Result<Output, Error>  {
+        Command::new("cpar")
+			.arg(args.input_path)
+			.arg(args.output_path)
+			.output()
+    }
+
+	fn create_test_files(files: Vec<&str>) {
+		//
+		for filename in files
+	}
+
+    #[test]
+    fn only_accepts_args_with_variable() {
+        let args = Args {
+            input_path: "fake-file.js".to_string(),
+            output_path: "fake-file.ts".to_string(),
+        };
+
+		if run_cpar(args).is_ok() { panic!() }
+    }
+
+	// test case for name.ext -> new_name.ext
+	fn copies_multiple_names() {
+		// creates test directories
+		fs::create_dir(".test-cache/").unwrap();
+		fs::write(".test-cache/temp-1.js", r#"// not intended for use"#).unwrap();
+		fs::write(".test-cache/temp-2.js", r#"// not intended for use"#).unwrap();
+
+		let args = Args {
+            input_path: "fake-file.js".to_string(),
+            output_path: "fake-file.ts".to_string(),
+        };
+
+		if run_cpar(args).is_ok() { panic!() }
+	}
 }
